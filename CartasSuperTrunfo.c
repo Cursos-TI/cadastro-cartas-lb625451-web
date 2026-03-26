dc05042b994f13aa90e3432558869694b471d5df
lb625451-web <lb625451@gmail.com>
2026-03-26 17:07:42 +0000

    segunda carat

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,18 +3,18 @@
 
 int main() {
 //variaveis da primeira carta;
-char estado1=a;
-char carta1[4]=a02;
-char nome1[20]=igapora;
-int populacao1.turismo1=1000;
+char estado1='a';
+char carta1[4]='a02';
+char nome1[20]="igapora";
+int populacao1,turismo1=1000;
 float area1=100.228;
 float pib1=10937.1;
 
 //variaveis da segunda carta 
- char estado2=a;
-char carta2[4]=a02;
-char nome2[20]=igapora;
-int populacao2.turismo2=1000;
+ char estado2='a';
+char carta2[4]="a02";
+char nome2[20]="igapora";
+int populacao2,turismo2=1000;
 float area2=100.228;
 float pib2=10937.1;
 
@@ -26,10 +26,10 @@ scanf("%s",&nome1);
 printf("Escreva uma letra de 'A a h '(representando um dos oitos estados ): ");
 scanf("%c",&estado1);
 printf("Escreva a letra do estado seguida de um numero de 01 a 04:" );
-scanf("%s",&carta11);
+scanf("%s",&carta1);
 printf("Escreva o número de habitantes da cidade: ");
 scanf("%d",&populacao1);
-printf("Escreva A área da cidade em quilômetros quadrad;os:")
+printf("Escreva A área da cidade em quilômetros quadrad;os:");
 scanf("%f",&area1);
 printf("Escreva o produto interno bruto da cidade:  ");
 scanf("%f",&pib1);
@@ -37,7 +37,7 @@ printf("Escreva A quantidade de pontos turísticos na cidade:");
 scanf("%d",&turismo1);
 
  //recolhendao os dados da segunda carta
- 
+
 printf("Carta [02]");
 printf("Escreva o nome da cidade: ");
 scanf("%s",&nome2);
@@ -47,7 +47,7 @@ printf("Escreva a letra do estado seguida de um numero de 01 a 04:" );
 scanf("%s",&carta2);
 printf("Escreva o número de habitantes da cidade: ");
 scanf("%d",&populacao2);
-printf("Escreva A área da cidade em quilômetros quadrad;os:")
+printf("Escreva A área da cidade em quilômetros quadrad;os:");
 scanf("%f",&area2);
 printf("Escreva o produto interno bruto da cidade:  ");
 scanf("%f",&pib2);
