b997bfbf9c921ccb0c408797721f26fd75ea2fba
lb625451-web <lb625451@gmail.com>
2026-03-26 16:57:13 +0000

    colocando informaçoes da segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,43 +2,79 @@
 
 
 int main() {
-char estado=a;
-char carta[4]=a02;
-char nome[20]=igapora;
-int populacao.turismo=1000;
-float area=100.228;
-float pib=10937.1;
- 
+//variaveis da primeira carta;
+char estado1=a;
+char carta1[4]=a02;
+char nome1[20]=igapora;
+int populacao1.turismo1=1000;
+float area1=100.228;
+float pib1=10937.1;
 
+//variaveis da segunda carta 
+ char estado2=a;
+char carta2[4]=a02;
+char nome2[20]=igapora;
+int populacao2.turismo2=1000;
+float area2=100.228;
+float pib2=10937.1;
+
+//recolhendo dados da primeira carta 
 
 printf("Carta [01]");
 printf("Escreva o nome da cidade: ");
-scanf("%s",&nome);
+scanf("%s",&nome1);
 printf("Escreva uma letra de 'A a h '(representando um dos oitos estados ): ");
-scanf("%c",&estado);
+scanf("%c",&estado1);
 printf("Escreva a letra do estado seguida de um numero de 01 a 04:" );
-scanf("%s",&carta);
+scanf("%s",&carta11);
 printf("Escreva o número de habitantes da cidade: ");
-scanf("%d",&populacao);
+scanf("%d",&populacao1);
 printf("Escreva A área da cidade em quilômetros quadrad;os:")
-scanf("%f",&area);
+scanf("%f",&area1);
 printf("Escreva o produto interno bruto da cidade:  ");
-scanf("%f",&pib);
+scanf("%f",&pib1);
 printf("Escreva A quantidade de pontos turísticos na cidade:");
-scanf("%d",&turismo);
+scanf("%d",&turismo1);
 
+ //recolhendao os dados da segunda carta
+ 
+printf("Carta [02]");
+printf("Escreva o nome da cidade: ");
+scanf("%s",&nome2);
+printf("Escreva uma letra de 'A a h '(representando um dos oitos estados ): ");
+scanf("%c",&estado2);
+printf("Escreva a letra do estado seguida de um numero de 01 a 04:" );
+scanf("%s",&carta2);
+printf("Escreva o número de habitantes da cidade: ");
+scanf("%d",&populacao2);
+printf("Escreva A área da cidade em quilômetros quadrad;os:")
+scanf("%f",&area2);
+printf("Escreva o produto interno bruto da cidade:  ");
+scanf("%f",&pib2);
+printf("Escreva A quantidade de pontos turísticos na cidade:");
+scanf("%d",&turismo2);
+ 
+//saia de dados da primeira carta  
 
+printf("carta 01:");
+printf(" Nome da cidade:%s",&nome1 );
+printf(" Estado:%c",&estado1 );
+printf(" Códogo:%s",&carta1 );
+printf(" População:%d",&populacao1 );
+printf(" Área:%f",&area1);
+printf(" PIB:%f",&pib1 );
+printf("Numero de pontos turísticos:%d",&turismo1 );
 
-printf(" Nome da cidade:%s",&nome );
-printf(" Estado:%c",&estado );
-printf(" Códogo:%s",&carta );
-printf(" População:%d",&populacao );
-printf(" Área:%f",&area );
-printf(" PIB:%f",&pib );
-printf("Numero de pontos turísticos:%d",&turismo );
+//saida de dados da segunda carta
 
-
-
+printf("carta 02:");
+printf(" Nome da cidade:%s",&nome2 );
+printf(" Estado:%c",&estado2 );
+printf(" Códogo:%s",&carta2 );
+printf(" População:%d",&populacao2 );
+printf(" Área:%f",&area2 );
+printf(" PIB:%f",&pib2 );
+printf("Numero de pontos turísticos:%d",&turismo2 );
 
 
 
